bfce20191160fc3553aa6701f0ba6f6c7f9071a6
Yeshua Miguel B. Abrenica <abrenica.y@gmail.com>
2023-07-15 21:11:34 +0800

    finalized login

--- index.php
+++ index.php
@@ -58,22 +58,120 @@ try {
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+
+
 
 *{
   padding:0;
   margin:0;
 }
 body,html{
-  background:#2b246a;
+  background:#B92A30;
  position:relative;
 }
 .logInBackground{
- background: url('dist/img/logofade.png'); 
+
  background-size: cover;
  background-repeat: no-repeat;
- width: 100%;
- height: 100vh;
+width: 100%;
+display: flex;
+
+justify-content: center;
 }
+
+.loginCard{
+  background:#2a246a;
+
+  margin-top:13em;
+ box-shadow: 0 3px 3px rgb(0,0,0,0.6);
+}
+
+
+.loginCardBackground{
+  display: flex;
+  padding:2em 4em;
+  background:url('dist/img/logofade.png');
+  background-size: cover;
+}
+.loginCardRow{
+  width: 100%;
+  padding:4em 2em;
+}
+
+.logoHolder{
+  display: flex;
+  margin-right: 3em;
+
+}
+
+.title{
+  margin-top:4.5em;
+}
+
+.logoHolder p{
+  font-family: 'Nunito', sans-serif;
+  color:white;
+  font-size: 10px;
+  margin-bottom: -10px;
+}
+.logoHolder h3{
+  font-family: 'Anton', sans-serif;
+  
+  text-transform: uppercase;
+  letter-spacing:2px;
+  font-size: 2em;
+}
+
+.formHolder{
+  width:40%;
+}
+
+
+.formHolder h4{
+  color:white;
+  font-family: 'Nunito', sans-serif;
+  font-size: 15px;
+}
+.loginCardRow img{
+  margin-top:1em;
+  height:120px;
+ 
+  padding:1.5em;
+}
+
+.loginCardRow h3{
+  color:white;
+}
+
+.inputcon{
+  margin-bottom: 1em;
+  margin-top:10px;
+}
+
+.inputcon input{
+  font-size:17px;
+  padding:10px;
+}
+
+.inputconButton{
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.submitButton{
+  background:#B92A30;
+  border:none;
+  padding:7px;
+  text-transform: uppercase;
+  color:white;
+  font-weight: 900;
+  font-family: 'Nunito', sans-serif;
+  cursor:pointer;
+}
+
 </style>
 
 
@@ -84,6 +182,21 @@ body,html{
 
 
 
+<div class="loginCard">
+<div class="loginCardBackground">
+
+
+<div class="loginCardRow logoHolder">
+
+  <img src="dist/img/logo2.png"> 
+  <div class="title">
+  <p>Barangay</p><h3>Anabu 1-E</h3>
+  </div>
+</div>
+
+
+<div class="loginCardRow formHolder">
+<h4>Login to your PMS</h4>
 <form method="post">
       <div class="col-md-12">
             <p class="wrongInput">
@@ -104,12 +217,28 @@ body,html{
           </div>
         
 
-        <div class="inputcon">
+        <div class="inputconButton">
             <button name="login" type="submit" class="submitButton">Sign In</button>
 
         </div>
 
 </form>
+
+</div>
+
+
+</div>
+
+
+
+
+</div>
+
+
+
+
+
+
 
 
 
